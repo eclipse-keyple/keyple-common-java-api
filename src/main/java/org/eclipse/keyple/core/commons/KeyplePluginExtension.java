cee9a99144b1de1613b2da82902632f672114f6f
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2021-02-12 15:57:50 +0100

    Improve build process and add interfaces

---
+++ src/main/java/org/eclipse/keyple/core/commons/KeyplePluginExtension.java
@@ -0,0 +1,23 @@
+/* **************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.core.commons;
+
+/**
+ * Generic type for a Keyple plugin extension.
+ *
+ * <p>A plugin extension brings the specific attributes of a plugin.
+ *
+ * <p>Any Keyple plugin must provide a factory that returns plugins implementing this interface.
+ *
+ * @since 2.0
+ */
+public interface KeyplePluginExtension {}
--- src/main/java/org/eclipse/keyple/core/commons/KeyplePluginFactory.java
+++
@@ -1,4 +0,0 @@
-package org.eclipse.keyple.core.commons;
-
-public interface KeyplePluginFactory {
-}
